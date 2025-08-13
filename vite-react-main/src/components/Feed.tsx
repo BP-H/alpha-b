f67efd0594dcf899a0f158b4f40df63a7bb090c6
BP-H <blueprinthustler@gmail.com>
2025-08-12 23:52:38 -0400

    Update Feed.tsx

--- vite-react-main/src/components/Feed.tsx
+++ vite-react-main/src/components/Feed.tsx
@@ -1,26 +1,2 @@
-import { Post, User } from "../types";
-import PostCard from "./PostCard";
-import "./feed.css";
-
-type Props = {
-  posts: Post[];
-  me: User;
-  onOpenProfile?: (id: string) => void;
-};
-
-export default function Feed({ posts, me, onOpenProfile }: Props) {
-  return (
-    <div className="content-viewport feed-wrap">
-      <div className="feed-content">
-        {posts.map((p) => (
-          <PostCard
-            key={p.id}
-            post={p}
-            me={me}
-            onOpenProfile={onOpenProfile}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
+// Delegate to the mobile-only Feed to avoid prop drift.
+export { default } from "./feed/Feed";
