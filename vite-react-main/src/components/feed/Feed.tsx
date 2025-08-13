b0f1d3a7241d4c60f76f872307faf323f433ec41
BP-H <blueprinthustler@gmail.com>
2025-08-12 23:44:58 -0400

    Update Feed.tsx

--- vite-react-main/src/components/feed/Feed.tsx
+++ vite-react-main/src/components/feed/Feed.tsx
@@ -2,14 +2,14 @@ import { Post, User } from "../../types";
 import PostCard from "../PostCard";
 import "./Feed.css";
 
-type Props = { posts: Post[]; me: User; onOpenProfile?: (id: string) => void };
+type Props = { posts: Post[]; me: User; onOpenProfile?: (id: string) => void; onEnterWorld: () => void };
 
-export default function Feed({ posts, me, onOpenProfile }: Props) {
+export default function Feed({ posts, me, onOpenProfile, onEnterWorld }: Props) {
   return (
     <div className="content-viewport feed-wrap">
       <div className="feed-content">
         {posts.map((p) => (
-          <PostCard key={p.id} post={p as Post} me={me as User} onOpenProfile={onOpenProfile} />
+          <PostCard key={p.id} post={p} me={me} onOpenProfile={onOpenProfile} onEnterWorld={onEnterWorld} />
         ))}
       </div>
     </div>
